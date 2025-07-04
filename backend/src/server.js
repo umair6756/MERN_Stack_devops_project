39d65ce548c55429743daa474cad1e79e720f28f
Hafiz Umair <chumair6756@gmail.com>
2025-07-04 03:09:05 -0400

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -33,7 +33,7 @@ for (const filePath of modelsFiles) {
 
 // Start our app!
 const app = require('./app');
-app.set('port', process.env.PORT || 8888);
+app.set('port', process.env.PORT || 5000);
 const server = app.listen(app.get('port'), () => {
   console.log(`Express running → On PORT : ${server.address().port}`);
 });
